07d100fa316aa244eedfcbcb522cd1e5a1f67a59
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-09-28 02:13:10 -0400

    Added navigation bar

--- src/components/Navigation/Navigation.component.jsx
+++ src/components/Navigation/Navigation.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navigation.styles.scss';
+import image from '../../assets/image_one.png';
 
 function Navigation() {
 	return (
@@ -53,27 +54,68 @@ function Navigation() {
 			</div>
 			<div className='column is-8 mt-3'>
 				<nav className='navbar' role='navigation' aria-label='main navigation'>
-					<div class='navbar-brand'>
-						<a class='navbar-item' href='https://bulma.io'>
-							<img
-								src='https://bulma.io/images/bulma-logo.png'
-								alt='Bulma: Free, open source, and modern CSS framework based on Flexbox'
-								width='112'
-								height='28'
-							/>
-						</a>
+					<div className='nav-flex'>
+						<div className='nav-left'>
+							<div class='navbar-brand'>
+								<a class='navbar-item' href='https://localhost:3000'>
+									<img src={image} alt='product feedback logo' />
+								</a>
 
-						<a
-							role='button'
-							class='navbar-burger'
-							aria-label='menu'
-							aria-expanded='false'
-							href='google'
-						>
-							<span aria-hidden='true'></span>
-							<span aria-hidden='true'></span>
-							<span aria-hidden='true'></span>
-						</a>
+								<a
+									role='button'
+									class='navbar-burger'
+									aria-label='menu'
+									aria-expanded='false'
+									href='google'
+								>
+									<span aria-hidden='true'></span>
+									<span aria-hidden='true'></span>
+									<span aria-hidden='true'></span>
+								</a>
+							</div>
+							<p className='suggestions__amount'>6 Suggestions</p>
+							<div class='dropdown'>
+								<div class='dropdown-trigger'>
+									<button
+										class='button menu-btn'
+										aria-haspopup='true'
+										aria-controls='dropdown-menu'
+									>
+										<span className='dropdown__sort'>Sort by :</span>
+										<span className='dropdown__sorttype'>Most Upvotes</span>
+										<span class='icon is-small'>
+											<i class='fas fa-angle-down' aria-hidden='true'></i>
+										</span>
+									</button>
+								</div>
+								<div class='dropdown-menu' id='dropdown-menu' role='menu'>
+									<div class='dropdown-content'>
+										<a href='google' class='dropdown-item'>
+											Most Upvotes
+										</a>
+										<hr class='dropdown-divider' />
+										<a class='dropdown-item' href='google'>
+											Least Upvotes
+										</a>
+										<hr class='dropdown-divider' />
+										<a href='google' class='dropdown-item'>
+											Most Comments
+										</a>
+										<hr class='dropdown-divider' />
+										<a href='google' class='dropdown-item'>
+											Least Comments
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className='nav-right'>
+							<div className='buttons add-feedback'>
+								<button class='button is-add'>
+									<span className='add-feedback-plus'>+</span> Add Feedback
+								</button>
+							</div>
+						</div>
 					</div>
 				</nav>
 			</div>
